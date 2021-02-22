4efdd6dfa68f113ba51c56ac7084a0b82c71456d
Roman Sulymka <roma.sulimak@gmail.com>
2021-02-22 19:19:26 +0200

    add User repository

---
+++ src/main/java/edu/sulymka/registerofgraduates/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package edu.sulymka.registerofgraduates.repository;
+
+import edu.sulymka.registerofgraduates.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "select * from users where email =?1", nativeQuery = true)
+    User getUserByEmail(String email);
+}
