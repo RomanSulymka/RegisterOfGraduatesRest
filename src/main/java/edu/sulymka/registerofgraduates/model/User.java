5ddde51ef4fb321104b613f42823b1ea119ee71c
Roman Sulymka <roma.sulimak@gmail.com>
2021-03-05 19:09:58 +0200

    add spring security to User

--- src/main/java/edu/sulymka/registerofgraduates/model/User.java
+++ src/main/java/edu/sulymka/registerofgraduates/model/User.java
@@ -2,9 +2,12 @@ package edu.sulymka.registerofgraduates.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,7 +16,7 @@ import java.io.Serializable;
 @Data
 @Entity
 @Table(name = "users")
-public class User implements Serializable {
+public class User implements UserDetails {
     @Id
     @Column(nullable = false, updatable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,4 +30,37 @@ public class User implements Serializable {
     @JoinColumn(name = "role_id")
     private Role role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(role);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
